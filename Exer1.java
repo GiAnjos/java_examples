caef388529d79c51573bf872cce4680fbe08e120
Giovana Dos Anjos <93729823+GiAnjos@users.noreply.github.com>
2022-09-28 15:01:50 -0300

    Add files via upload

---
+++ Exer1.java
@@ -0,0 +1,30 @@
+// programa que inverte o vetor
+public class Exerc1 {
+
+    public static void main(String[] args) {
+        int i, j, aux;
+        int vet[] = { 1, 2, 3, 4, 5 };
+
+        System.out.println(" Vetor original: ");
+        for (i = 0; i < 5; i++) {
+            System.out.println(vet[i]);
+        }
+
+        // Bloco que inverte o vetor
+        j = 5 - 1;
+        i = 0;
+
+        while (i < j) {
+            aux = vet[i];
+            vet[i] = vet[j];
+            vet[j] = aux;
+            j--;
+            i++;
+        }
+
+        System.out.println("\n Vetor invertido: ");
+        for (i = 0; i < 5; i++) {
+            System.out.println(vet[i]);
+        }
+    }
+}
